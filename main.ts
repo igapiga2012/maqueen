f5e3c94f182bc01f045eba39a045ef936b1032a0
igapiga2012 <igawarzych@gmail.com>
2025-11-24 16:05:11 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
+maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 40)
+basic.pause(5000)
+maqueen.motorStop(maqueen.Motors.All)
 basic.forever(function () {
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 150)
-    basic.pause(500)
-    maqueen.motorStop(maqueen.Motors.All)
-    basic.pause(500)
+	
 })
